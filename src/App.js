da39a3c6e1c4ddb55560aa1853e1d1df0248599d
Umashankar Vengala <2200031870@kluniversity.in>
2024-03-12 08:09:49 +0530

    ZENE FE1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import './App.css'
+import Sidebar from './components/Sidebar';
+import Dashboard from './pages/Dashboard';
+import Trending from './pages/Trending';
+import Player from './pages/Player';
+import Favourites from './pages/Favourites';
+import Library from './pages/Library';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div id='app'>
+
+            <BrowserRouter>
+                <Sidebar >
+                  <Routes>
+
+                    <Route path="/" element={<Dashboard/>} />
+                    <Route path="/dashboard" element={<Dashboard/>} />
+                    <Route path="/trending" element={<Trending/>} />
+                    <Route path="/player" element={<Player/>} />
+                    <Route path="/favourites" element={<Favourites/>} />
+                    <Route path="/library" element={<Library/>} />
+
+                  </Routes>
+                </Sidebar>
+            </BrowserRouter>
+
+        </div>
+    );  
 }
 
 export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,69 @@
+import { MdDashboard, MdLibraryMusic } from 'react-icons/md'
+import { BsFire, BsFillMusicPlayerFill } from "react-icons/bs";
+import { TbMusicHeart } from "react-icons/tb";
+import { FaBars } from "react-icons/fa";
+import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+
+
+export default function Sidebar({children}) {
+
+    const [isOpen, setIsOpen] = useState(true)
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+
+    const menuItem = [
+        {
+            path:"/",
+            name:"Dashboard",
+            icon:<MdDashboard />
+        },
+        {
+            path:"/trending",
+            name:"Trending",
+            icon:<BsFire />
+        },
+        {
+            path:"/player",
+            name:"Player",
+            icon:<BsFillMusicPlayerFill />
+        },
+        {
+            path:"/favourites",
+            name:"Favourites",
+            icon:<TbMusicHeart />
+        },
+        {
+            path:"/library",
+            name:"Library",
+            icon:<MdLibraryMusic />
+        },
+    ]
+
+  return (
+    <div className="container" >
+        <div className="navbar"></div>
+        <div style={{width: isOpen ? "300px" : "85px"}} className="sidebar">
+            <div className="top_section">
+                <NavLink to="/" style={{display: isOpen ? "block" : "none"}} className='logo'>
+                    <img src='LOGO.png' alt='logo' className='logoimg' />
+                    {/* ZENE */}
+                </NavLink>
+                <div style={{marginLeft: isOpen ? "50px" : "15px"}} className='bars'>
+                    <FaBars onClick={toggle}/>
+                </div>
+            </div>
+            {
+                menuItem.map( (item,index)=> (
+                    <NavLink to={item.path} key={index} className="link" activeclassName="active">
+                        <div className="icon">{item.icon}</div>
+                        <div className="link_text">{item.name}</div>
+                    </NavLink>
+                ))
+            }
+        </div>
+        <main>{children}</main>
+    </div>
+  )
+}
