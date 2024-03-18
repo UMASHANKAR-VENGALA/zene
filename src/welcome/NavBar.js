6496ef0dd3e710ddcf4d1597c87874811be2f6ba
Umashankar Vengala <2200031870@kluniversity.in>
2024-03-18 22:58:47 +0530

    viewusers table

--- src/welcome/NavBar.js
+++ src/welcome/NavBar.js
@@ -1,13 +1,8 @@
 /* eslint-disable jsx-a11y/no-redundant-roles */
 import React from 'react'
-import {  Link, Route, Routes } from 'react-router-dom';
+import {  Link } from 'react-router-dom';
 import logo from "./LOGO.png"
-import Register from './Register';
-import SignIn from './SignIn';
-// import Button from '@mui/material/Button';
-import Home from './Home';
-import DashBoard from '../user/Dashboard';
-import AdminLogin from './../admin/AdminLogin';
+
 import './SignIn.css'
 
 
@@ -34,15 +29,6 @@ export default function NavBar() {
                   </div>
                   
                 </nav>
-        
-            {/* <Routes>
-                <Route path='/' element={<Home/>} />
-                <Route path='/signin' element={<SignIn/>} />
-                <Route path='/register' element={<Register/>}/>
-                <Route path='/dashboard' element={<DashBoard/>}/>
-                <Route path='/adminlogin' element={<AdminLogin/>}/>
-            </Routes> */}
-        
         
     </div>
   )
